c916cc16294173338385989f5c6803397bfeefed
Nelson Sosa <nelsonrsavallone@gmail.com>
2020-03-30 15:53:23 -0300

    Nelson Commit 2

--- MatiasTest/src/test/java/com/matias/test/NelsonClass.java
+++ MatiasTest/src/test/java/com/matias/test/NelsonClass.java
@@ -1,13 +1,20 @@
 package com.matias.test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class NelsonClass {
 
 	private WebDriver driver;
 	
 	public void clickEntregaDePedido () {
+		By btnEntrega = By.xpath("/html/body/div[1]/div[1]/ng-include/div/div/section/div/button");		
 		
+		if(driver.findElement(btnEntrega).isDisplayed()) {
+			
+			driver.findElement(btnEntrega).click();
+		}
 		
 	}
 	
