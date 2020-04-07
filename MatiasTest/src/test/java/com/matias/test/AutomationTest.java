7776052958a0cb69b12a7108a881056604aa59ef
Matias Barrera <matiastumbarello@gmail.com>
2020-04-07 09:59:33 -0300

    Solucion iFrame en FranciscoClass

--- MatiasTest/src/test/java/com/matias/test/AutomationTest.java
+++ MatiasTest/src/test/java/com/matias/test/AutomationTest.java
@@ -25,9 +25,9 @@ public class AutomationTest {
 		
 		//driver.get("https://test1dom--sittest.lightning.force.com/lightning/r/Account/001c000002JvBrCAAV/view?ws=%2Flightning%2Fr%2FOrder%2F8013K000000EBx3QAG%2Fview");
 
-		driver.get("https://test1dom--sittest.my.salesforce.com/secur/frontdoor.jsp?sid=00D3K0000008jQa!ARwAQOZdtFPVElvPBnQq4TB.5xkdEErwSlw3bOWrfigUGlkaYb.x2Hm8vOssDItMm.EGIv5wtIJt3rUcLvxCARyz6GLqzpjD");
+		driver.get("https://test1dom--sittest.my.salesforce.com/secur/frontdoor.jsp?sid=00D3K0000008jQa!ARwAQAkZimZFoW91SqVre0eLSivQc4qRDpfYplG2E.4LnJotp2lkx7oTzDZb2txh1qxfjSh0x91qS8mqSc1WWA_MSbTPg6G5");
 
-		driver.get("https://test1dom--sittest.lightning.force.com/lightning/r/Account/001c000002JvBrCAAV/view?ws=%2Flightning%2Fr%2FOrder%2F8013K000000EBx3QAG%2Fview");
+		driver.get("https://test1dom--sittest.lightning.force.com/lightning/r/Account/0013K000005YuoUQAS/view");
 
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		
