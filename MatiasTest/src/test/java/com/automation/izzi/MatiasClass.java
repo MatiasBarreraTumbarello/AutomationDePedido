ada5b03dc9879f717c64cd0207c60d0b2b9d67e3
Matias Barrera <matiastumbarello@gmail.com>
2020-04-08 13:08:37 -0300

    Ignore commit

--- MatiasTest/src/test/java/com/automation/izzi/MatiasClass.java
+++ MatiasTest/src/test/java/com/automation/izzi/MatiasClass.java
@@ -34,21 +34,25 @@ public class MatiasClass{
 	
 	//Este metodo se utiliza en el primer paso del flujo de portabilidad
 	public static void codigoNIP(WebDriver driver) throws InterruptedException{
+		//*[@id="RadioNIP|0"]/div/div[1]/label[1]
+		
 		WebDriverWait  wait = new WebDriverWait (driver, 40);
 		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("slds-spinner_container")));
 		List<WebElement> cantIFrames = driver.findElements(By.tagName("iFrame"));
 		int size = cantIFrames.size();
-		//driver.switchTo().frame(size - 1);
-		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(size - 1));
+		driver.switchTo().frame("iFrameResizer3");
+		//wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(size - 1));
 		Thread.sleep(2000);
 		
 		//Descomentar cualquiera de las dos siguientes lineas, de acuerdo a la respuesta que desea colocar
 		
 		//Esta opcion afirma que el cliente cuenta con un nro NIP
-		WebElement myDynamicElement = 
-				(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.id("RadioNIP")));
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//input[@id='RadioNIP']")));
-		driver.findElement(By.xpath("//input[@id='RadioNIP' and @value='Si']")).click();
+		//WebElement myDynamicElement = 
+		//		(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.id("RadioNIP")));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form[@id='a2c3K0000000hGOQAY-4']")));
+		List<WebElement> boton = driver.findElements(By.xpath("/html/body/span/div/span/div/ng-view/div/div/bptree/child[4]/div/section/form/div[1]/div/child[1]/div/ng-form/div/div[1]"));
+		Thread.sleep(2000);
+		boton.get(0).findElement(By.xpath("./..")).click();
 		
 		//Esta opcion niega que el cliente cuenta con un nro NIP
 		//driver.findElement(By.xpath("//input[@id='RadioNIP' and @value='No']")).click();
