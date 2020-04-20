b0f0b1856d54f7c3db44ad7a841bf7084f404087
faguilera1952 <franciscoaguilera1990@gmail.com>
2020-04-20 16:36:03 -0300

    ignore

---
+++ MatiasTest/src/test/java/com/automation/izzi/ProcesoCambioDeSim.java
@@ -0,0 +1,39 @@
+package com.automation.izzi;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ProcesoCambioDeSim {
+	
+	private WebDriver driver;
+	
+
+	@Before
+	public void setUp() throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://test1dom--sittest.my.salesforce.com/secur/frontdoor.jsp?sid=00D3K0000008jQa!ARwAQBVfkMth.4rmDYSVQh8fTRP2DFn.P_dufeTxawAJEt93vuuJSeyfluWd5G51mNdu2sD1au99YrXo3Pr.TSoLM5KuxH1m");
+
+		driver.get("https://test1dom--sittest.lightning.force.com/lightning/r/Account/001c000002JvBrCAAV/view");
+
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		
+		Thread.sleep(20000);
+		
+	}
+	
+	@Test
+	public void testScript() throws InterruptedException {
+	
+		MatiasClass.cambioDeSim(driver);
+		MatiasClass.seleccioneSim(driver);
+	}
+
+}
