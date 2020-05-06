c14a0943b9ec2f05be8463f110c2a868319dbf0a
Nelson Sosa <nelsonrsavallone@gmail.com>
2020-05-06 11:58:08 -0300

    Cambio de SIM Id en ICCID y botones

--- MatiasTest/src/test/java/com/automation/izzi/ProcesoCambioDeSim.java
+++ MatiasTest/src/test/java/com/automation/izzi/ProcesoCambioDeSim.java
@@ -58,22 +58,22 @@ public class ProcesoCambioDeSim {
 		config.waitForInvisibleSpinner();
 
 
-		WebElement iframe = wait.until(ExpectedConditions.elementToBeClickable(By.id("iFrameResizer3")));
+		WebElement iframe = new WebDriverWait(driver,40).until(ExpectedConditions.elementToBeClickable(By.id("iFrameResizer3")));
 		iframe.click();
 		driver.switchTo().frame(iframe);
 
 		Thread.sleep(2000);
 
-		WebElement opcion = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"ChooseSim\"]/div/ng-include/div/div[2]/ul/li")));
+		WebElement opcion = new WebDriverWait(driver,40).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"ChooseSim\"]/div/ng-include/div/div[2]/ul/li")));
 		opcion.click();
-		driver.findElement(By.xpath("//*[@id=\'Step1_nextBtn\']")).click();
+		driver.findElement(By.xpath("//div[@id='SIMSelection_nextBtn']")).click();
 		//Paso3
 		config.waitForInvisibleSpinner();
 		
-		driver.findElement(By.xpath("//*[@id=\'newIMSI\']")).sendKeys("123456789101113");
+		driver.findElement(By.xpath("//input[@id='ICCID']")).sendKeys("12345678912345678912");
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//input[@id=\'newICCID\']")).sendKeys("12345678910111213143");
-		driver.findElement(By.xpath("//*[@id=\'Step2_nextBtn\']")).click();
+		driver.findElement(By.xpath("//*[@id='ICCIDVal']")).sendKeys("12345678910111213143");
+		driver.findElement(By.xpath("//div[@id=\'DeliverySimCard_nextBtn\']")).click();
 		//Paso4
 		config.waitForInvisibleSpinner();
 		
