9fa39d79077f5d054b17e427dfbd6439f6e890a1
Matias Barrera <matiastumbarello@gmail.com>
2020-05-07 16:18:05 -0300

    Se arreglan y optimzan las clases de Nueva venta y entrega de pedido

--- MatiasTest/src/test/java/com/automation/izzi/ProcesoAltaDeServicios.java
+++ MatiasTest/src/test/java/com/automation/izzi/ProcesoAltaDeServicios.java
@@ -19,107 +19,124 @@ public class ProcesoAltaDeServicios {
 	private Main main = new Main();
 	private WebDriver driver;
 	private WebDriverWait wait;
-	
-	
+
 	@Before
 	public void setUp() throws InterruptedException, IOException {
-		
-		
+
 		driver = main.setDriver();
 		main.initBrowser();
 		main.goToAccountLink();
 
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-		
+
 		Thread.sleep(20000);
-		
+
 	}
-	
+
 	@Test
 	public void testScript() throws InterruptedException {
-		try {
-			llamadosDeMetodos();
-		} catch (Exception e) {
-			main.returnExecutionError(getClass().getName());
-		}
 		
+		try {
+			AltaDeServicios();
+		} catch (InterruptedException e	) {
+			//main.returnExecutionError(getClass().getName());
+			
+		}
+
 	}
 
 	public void AltaDeServicios() throws InterruptedException {
 		Thread.sleep(2000);
 		WebElement frame = driver.findElement(By.id("iFrameResizer2"));
-		JavascriptExecutor executor = (JavascriptExecutor)driver;
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
 		executor.executeScript("arguments[0].style.display = 'block'; arguments[0].style.zIndex = '999999';", frame);
 		driver.switchTo().frame(frame);
 		Thread.sleep(2000);
 		List<WebElement> links = driver.findElements(By.linkText("Alta de Servicios"));
 		executor.executeScript("arguments[0].click();", links.get(2));
-		//links.get(0).click();
+		// links.get(0).click();
 		Thread.sleep(2000);
+		
+		SeleccionDePlan();
 	}
-	
+
 	public void SeleccionDePlan() throws InterruptedException {
 		Thread.sleep(4000);
 		driver.switchTo().defaultContent();
 		WebElement frame = driver.findElement(By.id("iFrameResizer3"));
 		driver.switchTo().frame(frame);
-		
+
 		main.waitForInvisibleSpinner();
 		WebElement plan = new WebDriverWait(driver, 40)
-		    	.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='block_0']")));
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='block_0']")));
 		plan.findElement(By.xpath("./..")).click();
-		
+
 		main.waitForInvisibleSpinner();
-		new WebDriverWait (driver, 40)
+		new WebDriverWait(driver, 40)
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='ChooseAndAddProducts_nextBtn']")));
 		driver.findElement(By.xpath("//div[@id='ChooseAndAddProducts_nextBtn']")).click();
 
 		main.waitForInvisibleSpinner();
 		Thread.sleep(2000);
+		
+		confirmarServicio();
 	}
-	
-	public void confirmarServicio() throws InterruptedException{
-		//Seccion: Confirmacion
-		main.waitForInvisibleSpinner();
-		new WebDriverWait(driver, 40).until(ExpectedConditions.elementToBeClickable(By.id("RadioConfirmation")));
-		List<WebElement> opcion = driver.findElements(By.id("RadioConfirmation"));
-		
-		//En caso de seleccionar la opcion de NO, descomentar la siguiente linea, por defecto se selecciona SI
-		//opcion.get(1).findElement(By.xpath("../.")).click();
-	
-		main.waitForInvisibleSpinner();
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("Confirmation_nextBtn")));
-		Thread.sleep(2000);
-		driver.findElement(By.id("Confirmation_nextBtn")).click(); //boton siguiente
-		Thread.sleep(3000);
-		
-		//Seccion: Resumen de compra
-	
-		main.waitForInvisibleSpinner();
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("DeliveryHomeSummary_nextBtn")));
-		driver.findElement(By.xpath("//*[@id='DeliveryHomeSummary_nextBtn']/p")).click();
-	
-		Thread.sleep(3000);
-		
-		main.waitForInvisibleSpinner();
-		
-		Thread.sleep(3000);
-		
-		
-		//Finalizar
-		
-		/*driver.findElement(By.xpath("//button[@class='slds-button slds-button_brand ng-binding' and contains(text(),Finalizar)]")).click();
-		Thread.sleep(3000);*/
-		
-		
-		
 
+	public void confirmarServicio() throws InterruptedException {
+		// Seccion: Confirmacion
+		main.waitForInvisibleSpinner();
+		new WebDriverWait(driver, 40)
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//ng-form[@id='RadioConfirmation|0']")));
+		List<WebElement> opcion = driver.findElements(By.id("RadioConfirmation"));
+
+		// En caso de seleccionar la opcion de NO, descomentar la siguiente linea, por
+		// defecto se selecciona SI
+		// opcion.get(1).findElement(By.xpath("../.")).click();
+
+		main.waitForInvisibleSpinner();
+		new WebDriverWait(driver, 40)
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='Confirmation_nextBtn']")));
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@id='Confirmation_nextBtn']")).click(); // boton siguiente
+		Thread.sleep(3000);
+
+		// Seccion: Resumen de compra
+
+		main.waitForInvisibleSpinner();
+		new WebDriverWait(driver, 40)
+				.until(ExpectedConditions.elementToBeClickable(By.id("DeliveryHomeSummary_nextBtn")));
+		driver.findElement(By.xpath("//*[@id='DeliveryHomeSummary_nextBtn']/p")).click();
+
+		Thread.sleep(3000);
+
+		main.waitForInvisibleSpinner();
+
+		Thread.sleep(3000);
+
+		new WebDriverWait(driver, 20).until(ExpectedConditions
+				.elementToBeClickable(By.xpath("//div[@ng-if='control.propSetMap.structMessage.btnName']")));
+
+		String orden = driver.findElement(By.xpath("//p[@ng-repeat='(key, value) in control.propSetMap.message']"))
+				.getText();
+		driver.switchTo().defaultContent();
+		String letra = "";
+		for (int i = 5; i < orden.length(); i++) {
+			letra = letra + orden.charAt(i);
+		}
+		driver.findElement(By.xpath("//input[@id='159:0;p']")).sendKeys(letra);
+	/*	List<WebElement> desplegable = driver
+				.findElements(By.xpath("//li[@data-aura-class='uiAutocompleteOption forceSearchInputDesktopOption']"));
+	//	new WebDriverWait(driver, 40).until(ExpectedConditions.textToBePresentInElement(desplegable.get(1), letra));
+		Thread.sleep(2000);
+		desplegable.get(1).click();*/
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id=\'oneHeader\']/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div/div/div[2]/ul/li[2]")).click();
 	}
-	
-	public void llamadosDeMetodos ()throws InterruptedException {
-	//	IrACuenta(driver);
+
+/*	public void llamadosDeMetodos() throws InterruptedException {
+		// IrACuenta(driver);
 		AltaDeServicios();
 		SeleccionDePlan();
 		confirmarServicio();
-	}
+	}*/
 }
